08ea9146215bef1f116da427d06a43c73ea5e838
Wyldson Marllon <wyldsonmarllon39@gmail.com>
2024-11-13 17:45:59 -0300

    Function Save

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+function converterCanvas(){
+    const canvas = document.createElement('canvas')
+    const ctx = canvas.getContext('2d')
+    const img = document.getElementById('imagemCarregada')
+    const {width, height} = img
+    canvas.width = width
+    canvas.height = height
+    ctx.drawImage(img, 0, 0, width, height)
+}
+
+
 function abrirArquivo(){
     const inputImg = document.getElementById('inputImage')
     inputImg.click()
@@ -7,10 +18,20 @@ function abrirArquivo(){
         leitor.onload = function() {
             const viewImg = document.getElementById('imagemCarregada')
             viewImg.src = leitor.result
+            converterCanvas()
         }
         leitor.readAsDataURL(arquivo)
     })
+
 }
 
+function salvarImagem(){
+    const link = document.createElement("a")
+    const img = document.getElementById('imagemCarregada')
+    link.download = "TESTE.JPG"
+    link.href = img.toDataURL()
+    link.click()
+}
 
 const btnOpen = document.getElementById('open').addEventListener('click', abrirArquivo)
+const btnSave = document.getElementById('save').addEventListener('click', salvarImagem)
