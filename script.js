cbf589ac0a8357c6bd2f970b875db735341bf8bd
Wyldson Marllon <wyldsonmarllon39@gmail.com>
2024-11-19 17:51:22 -0300

    Add uptadeFiltros and AplicarFiltros

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 const inputImage = document.getElementById('inputImg')
+const inputMedidor = document.getElementById('medidor')
+const msgMedidor = document.getElementById('msgmedidor')
+const msgFiltro = document.getElementById('msgfiltro')
+const imgCarregada = document.getElementById('imagemCarregada')
+const btnsFiltros = document.querySelectorAll('.filtros button')
+let brilho = 100, saturacao = 100, inversao = 0, escalacinza = 0
 
 function carregarImg(){
     const arquivo = inputImage.files[0]
     const leitor = new FileReader
     leitor.onload = function (){
-        const imgCarregada = document.getElementById('imagemCarregada')
         imgCarregada.src = leitor.result
     }
     leitor.readAsDataURL(arquivo)
 }
 
+function updateFiltros(){
+    msgMedidor.textContent = `${inputMedidor.value}%`
+}
+
+function aplicarFiltros(){
+    imgCarregada.style.filter = `brigthness(${brilho}) saturate(${saturacao}) invert(${inversao}) grayscale(${escalacinza})`
+}
+
+inputMedidor.addEventListener('input', updateFiltros)
 inputImage.addEventListener('change', carregarImg)
 const btnAbrirImg = document.getElementById('abrir').addEventListener('click',() => inputImage.click())
-
 
