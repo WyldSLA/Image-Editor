bb7c002eafcd878a483fb1a222d18874d3382f31
Wyldson Marllon <wyldsonmarllon39@gmail.com>
2024-11-20 14:42:54 -0300

    Final version (maybe)

--- script.js
+++ script.js
@@ -1,29 +1,94 @@
-const inputImage = document.getElementById('inputImg')
-const inputMedidor = document.getElementById('medidor')
-const msgMedidor = document.getElementById('msgmedidor')
-const msgFiltro = document.getElementById('msgfiltro')
-const imgCarregada = document.getElementById('imagemCarregada')
-const btnsFiltros = document.querySelectorAll('.filtros button')
-let brilho = 100, saturacao = 100, inversao = 0, escalacinza = 0
+const inputImage = document.getElementById('inputImg'),
+      inputMedidor = document.querySelector('.ajuste #medidor'),
+      valorMedidor = document.querySelector('.ajuste #valorMedidor'),
+      nomeFiltro = document.querySelector('.ajuste .nomeFiltro'),
+      imgCarregada = document.querySelector('.verImg #imagemCarregada'),
+      btnsFiltros = document.querySelectorAll('.btn-filtros button');
 
-function carregarImg(){
-    const arquivo = inputImage.files[0]
-    const leitor = new FileReader
-    leitor.onload = function (){
-        imgCarregada.src = leitor.result
+let brilho = 100, saturacao = 100, inversao = 0, escalacinza = 0;
+let filtroAtual = 'brilho';
+
+const aplicarFiltros = () => {
+    imgCarregada.style.filter = `
+        brightness(${brilho}%) 
+        saturate(${saturacao}%) 
+        invert(${inversao}%) 
+        grayscale(${escalacinza}%)
+    `;
+};
+
+const carregarImg = () => {
+    const arquivo = inputImage.files[0];
+    const leitor = new FileReader();
+    leitor.onload = function () {
+        imgCarregada.src = leitor.result;
+        resetarFiltros()
+    };
+    leitor.readAsDataURL(arquivo);
+};
+
+btnsFiltros.forEach(btn => {
+    btn.addEventListener('click', () => {
+        filtroAtual = btn.id; 
+        nomeFiltro.innerText = btn.innerText;
+
+        if (filtroAtual === "brilho") {
+            inputMedidor.max = "200"
+            inputMedidor.value = brilho;
+            valorMedidor.innerText = `${brilho}%`;
+        } else if (filtroAtual === "saturacao") {
+            inputMedidor.max = "200"
+            inputMedidor.value = saturacao;
+            valorMedidor.innerText = `${saturacao}%`;
+        } else if (filtroAtual === "inversao") {
+            inputMedidor.max = "100"
+            inputMedidor.value = inversao;
+            valorMedidor.innerText = `${inversao}%`;
+        } else{
+            inputMedidor.max = "100"
+            inputMedidor.value = escalacinza;
+            valorMedidor.innerText = `${escalacinza}%`;
+        }
+    });
+});
+
+const updateFiltros = () => {
+    valorMedidor.textContent = `${inputMedidor.value}%`;
+
+    if (filtroAtual === "brilho") {
+        brilho = inputMedidor.value;
+    } else if (filtroAtual === "saturacao") {
+        saturacao = inputMedidor.value;
+    } else if (filtroAtual === "inversao") {
+        inversao = inputMedidor.value;
+    } else{
+        escalacinza = inputMedidor.value;
     }
-    leitor.readAsDataURL(arquivo)
+
+    aplicarFiltros(); 
+};
+
+const resetarFiltros = () => {
+    brilho = 100, saturacao = 100, inversao = 0, escalacinza = 0;
+    filtroAtual = 'brilho';
+    aplicarFiltros();
 }
 
-function updateFiltros(){
-    msgMedidor.textContent = `${inputMedidor.value}%`
+const salvarImg = () => {
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+    canvas.width = imgCarregada.naturalWidth;
+    canvas.height = imgCarregada.naturalHeigth;
+    ctx.filter =  `brightness(${brilho}%) saturate(${saturacao}%) invert(${inversao}%) grayscale(${escalacinza}%)`
+    ctx.drawImage(imgCarregada, 0, 0, canvas.width, canvas.height)
+    const link = document.createElement("a")
+    link.download = "imagem-editada.png"
+    link.href = canvas.toDataURL("image/png")
+    link.click()
 }
 
-function aplicarFiltros(){
-    imgCarregada.style.filter = `brigthness(${brilho}) saturate(${saturacao}) invert(${inversao}) grayscale(${escalacinza})`
-}
-
-inputMedidor.addEventListener('input', updateFiltros)
-inputImage.addEventListener('change', carregarImg)
-const btnAbrirImg = document.getElementById('abrir').addEventListener('click',() => inputImage.click())
-
+inputMedidor.addEventListener('input', updateFiltros);
+inputImage.addEventListener('change', carregarImg);
+document.getElementById('abrir').addEventListener('click', () => inputImage.click());
+document.getElementById('resetar').addEventListener('click', resetarFiltros);
+document.getElementById('salvar').addEventListener('click', salvarImg)
